fb332df19e2cc2a6826dafaf46121e09556b5531
lukasx <llukasxx@gmail.com>
2015-05-18 05:02:25 -0700

    Apply and toying with CanCan gemgit add -A :)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150516143917) do
+ActiveRecord::Schema.define(version: 20150518111108) do
+
+  create_table "collaborations", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "collaborations", ["post_id"], name: "index_collaborations_on_post_id"
+  add_index "collaborations", ["user_id"], name: "index_collaborations_on_user_id"
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",   null: false
@@ -28,6 +48,7 @@ ActiveRecord::Schema.define(version: 20150516143917) do
     t.datetime "updated_at"
     t.string   "username"
     t.boolean  "is_active",              default: true
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+writer = User.create!(username: "writer", email: "writer@localhost.net", password: "Plumber364", 
+                      password_confirmation: "Plumber364")
+
+junior = User.create!(username: "junior", email: "junior@localhost.net",
+                      password: "Plumber364", 
+                      password_confirmation: "Plumber364")
+
+Post.create!(user: writer, title: "Hot Day",
+             content: "The sun seems to like me very much these days. I don't
+             really mind, but I really wish it could love someone else too.")
+
+Post.create!(user: junior, title: "Hello!",
+             content: "Hey! I'm pretty new here! Glad to be here! This is my
+                      first time here, so please be nice to me (and each other).")
